75cd8a763a0e9e6d395bd261f953a8158cf063bf
nishat-arif <nishatarif1912@gmail.com>
2025-07-03 18:15:18 -0400

    jsx and funtional component added

---
+++ basic.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("h1", {id:"heading"}, "Hello World from React")
+
+const jsxHeading = <h1 id="heading">Hello World from JSX</h1>;
+
+//const jsxHeading = (<h1 id="heading">Hello World from JSX</h1>);
+
+// const heading = React.createElement("h1", {id:"heading"}, 
+//                 React.createElement("h2", {id:"child"}, 
+//                 [React.createElement("h1", {id:"child1"}, "child1"),React.createElement("h1", {id:"child2"}, "child2") ]));
+// console.log("heading",typeof heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log("root",typeof root);
+root.render(jsxHeading);
