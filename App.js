6bda7b13d6bc3b22e32e2920f00b02ced44d3614
nishat-arif <nishatarif1912@gmail.com>
2025-07-06 23:49:43 -0400

    header component created

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { LOGO_URL } from "./utils/constants";   
-
-
-
+import { LOGO_URL} from "./utils/constants"; 
 
 const HeaderComponent = () => {
     return (
         <div className="header">
           <div className="logo">
-            <img src={LOGO_URL} alt="GrabIt Logo" />
+            <img src={LOGO_URL} alt="GrubExpress Logo" className="logo_img"></img>
         </div>
         <div className="navItems">
-            <ul>
+            <ul className="navList">
                 <li>Home</li>
                 <li>About</li>
-                <li>Contact</li>
+                <li>Contact</li> 
                 <li>Cart</li>
             </ul>
         </div>
@@ -27,11 +24,23 @@ const HeaderComponent = () => {
 const App =()=>{
     return (
         <div className="app">
-            <HeaderComponent />
-        
+            <HeaderComponent /> 
+            <BodyComponent />      
         </div>
     );
 }
+
+const BodyComponent = () => {
+    return (
+    <div>
+        <div className="search"></div>
+        <div className="restaurantList">
+            <h1>Restaurant List</h1>
+            {/* Add restaurant cards here */}
+        </div>
+
+    </div>
+    )};
 
 const root= ReactDOM.createRoot(document.getElementById("root"));
 
